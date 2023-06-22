1102fc9ce0c0c39c647491f5b0bbb0d5642c4b5e
gamjalee <jfamily0309@gmail.com>
2023-06-22 20:08:35 +0900

    텍스트 형식의 msg 배치 처리 및 주석 수정 완료

--- app.py
+++ app.py
@@ -16,21 +16,20 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 
+# MongoDB에 연결
 connStr = "mongodb+srv://cloudIOT2023:IOT2023@cluster0.fmzsuhg.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connStr)
-
 db = client['dms']
 coll = db["chat"]
 
 app = Flask(__name__)
 CORS(app)
 
+# 배치 처리 후 보내는 시간
 start_time = 21
 end_time = 9
-# 배치 처리 후 보내는 시간
 
-# 09~22시 사이에는 배치 레이어 작업
-# => 시간 체크하는 함수
+# 09~22시 사이에는 배치 레이어 작업 => 시간 체크하는 함수
 # check_time(): time is 09~22 => return True
 def check_time():
     time_now = datetime.datetime.now().hour
@@ -42,43 +41,44 @@ def check_time():
         print("True")
         return True
 
+
 # producer가 topic에 msg 전송
 def producerSend(writer, timestamp, content):
     producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
-    test_string={"writer":writer,"timestamp":str(timestamp),"content":content}
-    producer.send('test-topic',value=test_string)
+    test_string={"writer": writer, "timestamp": str(timestamp), "content": content}
+    producer.send('test-topic', value=test_string)
     producer.flush()
     result = consumer()
-    print("###############" , result)
+    print("###############", result)
 
 
 # consumer가 topic에서 msg 수신
 def consumerGet():
     consumer = KafkaConsumer('test-topic', bootstrap_servers='localhost:9092')
     for message in consumer:
-        # message : ConsumerRecord(topic='test-topic', partition=0, offset=73, timestamp=1683459269644, timestamp_type=0, key=None, value=b'{"writer": "testttttttest", "timestamp": "05/07 20:11", "content": "test content 0507"}', headers=[], checksum=None, serialized_key_size=-1, serialized_value_size=87, serialized_header_size=-1)
         value = message.value
         d = json.loads(value.decode('utf-8'))
         string = {"writer": d.get("writer", "Nothing"),"content": d.get("content", "Nothing"), "timestamp": d.get("timestamp", "Nothing")}
         return string
         
 
-
 # index route - 사용할 일 없음
 @app.route("/")
 def index():
     print("홈 들어옴")
     return "home"
-    
-# ui에서 msg 받아와 producerSend 메소드 실행
+
+
+# UI에서 msg 받아와 producerSend 메소드 실행
 @app.route("/msg_send",methods=['POST'])
 def producer_test():
     params = request.get_json()
     writer = params['writer']
     timestamp = params['timestamp']
     content = params['content']
+
+    # 09~22시 사이에 요청 보냄
     if check_time():
-    # 09~22시 사이에만 요청 보냄
         producerSend(writer, timestamp, content)
         return "ok"
     else:
@@ -89,8 +89,6 @@ def producer_test():
 
 @app.route("/img_send", methods=['POST'])
 def img_send():
-    # if check_time():
-    # 09~22시 사이에만 요청 보냄
     image = request.files["image"]
 
     # 이미지를 저장할 경로 설정
@@ -108,41 +106,24 @@ def img_send():
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)}), 500
-    
-    
-    
 
-
-
-"""
-@app.route("/msg_get",methods=['GET', 'POST'])
-def consumer_test():
-    ans = consumerGet()
-    # ans : {'content': 'test content 0507', 'timestamp': '05/07 20:11'} / <class 'dict'>
-    json_ans = json.dumps(ans)
-    # json_ans : {"content": "test content 0507", "timestamp": "05/07 20:11"} / <class 'str'>
-    # 모두 쌍따옴표로 감싸져 있어야 다시 javascript에서 JSON으로 변환 가능
-    return json_ans
-    
-""" 
 
 def consumer():
-    ans = consumerGet() ## 내 컴은 consumer가 안됨
-    # ans = '{"writer": "zeun", "timestamp": "06/09 15:43", "content": "h2hh2"}'
+    ans = consumerGet()
+    # 형식 ans = '{"writer": "zeun", "timestamp": "06/09 15:43", "content": "h2hh2"}'
     json_ans = json.dumps(ans)
     print(json_ans)
-    # ui 서버에 api 통해 json_ans 전달
+    # UI 서버에 api 통해 json_ans 전달
     url = 'http://localhost:5000/consumer'
 
+    # 09~22시 사이에 요청 보냄
     if check_time():
-    # 09~22시 사이에만 요청 보냄
         try:
             # 요청 데이터
             data = json_ans
 
             # API 요청 보내기
             response = requests.post(url, json=data)
-
 
             # 응답 처리
             if response.status_code == 200:
@@ -157,6 +138,5 @@ def consumer():
             # 예외 처리
             return 'API 요청 중 오류가 발생하였습니다: ' + str(e)
 
-    
 
 app.run(port=8989, host='localhost', debug=True)
--- mongoDB.py
+++ mongoDB.py
@@ -16,49 +16,52 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 
-connStr = "mongodb+srv://cloudIOT2023:IOT2023@cluster0.fmzsuhg.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(connStr)
+def batch_Get():
+    # MongoDB에 연결
+    connStr = "mongodb+srv://cloudIOT2023:IOT2023@cluster0.fmzsuhg.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(connStr)
 
-db = client['dms']
-coll = db["chat"]
+    db = client['dms']
+    coll = db["chat"]
 
-# document = {"name" : "name2", "age" : 65}
-# coll.insert_one(document)
+    # MongoDB 값 쿼리
+    docs = coll.find()
+    for doc in docs:
+        # send가 0일 경우 발송하지 않은 메세지
+        # send가 1일 경우 발송한 메세지
+        if doc['send'] == 0:
+            # producer에 채팅 작성자, 작성시간, 작성 내용 flush
+            producer = KafkaProducer(bootstrap_servers='3.135.130.17:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
+            test_string = {"writer": doc['writer'], "timestamp": doc['timestamp'], "content": doc['content']}
+            producer.send('test-topic', value=test_string)
+            producer.flush()
 
-docs = coll.find()
-for doc in docs:
-    # print(type(doc))
-    # print(type(doc['send']))
-    if doc['send'] == 0:
-        producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda x: dumps(x).encode('utf-8'))
-        test_string = {"writer": doc['writer'], "timestamp": doc['timestamp'], "content": doc['content']}
-        producer.send('test-topic', value=test_string)
-        producer.flush()
-        # print(test_string)
+            # consumer에 채팅 작성자, 작성시간, 작성 내용 받아오기
+            consumer = KafkaConsumer('test-topic', bootstrap_servers='3.135.130.17:9092')
+            json_ans = json.dumps(test_string)
+            print(f'json_ans: {json_ans}')
+            url = 'http://18.221.31.141:5000/consumer'
 
-        consumer = KafkaConsumer('test-topic', bootstrap_servers='localhost:9092')
-        json_ans = json.dumps(test_string)
-        print(f'json_ans: {json_ans}')
-        url = 'http://localhost:5000/consumer'
+            try:
+                # 요청 데이터
+                data = json_ans
+                # API 요청 보내기
+                response = requests.post(url, json=data)
+                # 응답 처리
+                if response.status_code == 200:
+                    # API 응답을 이용한 작업 수행
+                    result = response.json()
+                    # print(jsonify(result)) #NoneType으로 잡힘
+                else:
+                    print('API 요청이 실패하였습니다.')
+            except requests.exceptions.RequestException as e:
+                # 예외 처리
+                print('API 요청 중 오류가 발생하였습니다: ' + str(e))
 
-        try:
-            data = json_ans
-            # 요청 데이터
-            response = requests.post(url, json=data)
-            # API 요청 보내기
+            # 채팅 메세지 발송 이후 send를 1으로 업데이트하여 발송한 메세지로 표시
+            query = {'_id': doc['_id']}
+            new_values = {'$set': {'send': 1}}
+            coll.update_many(query, new_values)
 
-            # 응답 처리
-            if response.status_code == 200:
-                # API 응답을 이용한 작업 수행
-                result = response.json()
-                # print(jsonify(result)) #NoneType으로 잡힘
-            else:
-                print('API 요청이 실패하였습니다.')
-        except requests.exceptions.RequestException as e:
-            # 예외 처리
-            print('API 요청 중 오류가 발생하였습니다: ' + str(e))
 
-        # 발송 이후 업데이트
-        query = {'_id': doc['_id']}
-        new_values = {'$set': {'send': 1}}
-        coll.update_many(query, new_values)
+batch_Get()
